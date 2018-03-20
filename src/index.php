4c7e434796cd28df60e615a367484a1d2e57a4ca
MercanBrandon <mercan.brandon@outlook.fr>
2018-03-20 11:25:48 +0100

    début interface WEB

--- src/index.php
+++ src/index.php
@@ -3,13 +3,31 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="projetAnnuel/bootstrap-4.0.0/dist/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
     <title>TransportMe</title>
   </head>
-  <body>
-    <h1>Hello and welcome into my new application : TransportMe</h1>
+  <body id="index">
+    <header class="main">
+      <img src="img/LogoWhite.png" alt="Logo">
+      <nav>
+        <!-- <h1>L'application socialement responsable</h1> -->
+      </nav>
+    </header>
+    <div class="container">
+      <article class="connect">
+        <form class="form-connect" action="index.html" method="post">
+          <p><input type="text" name="mail" value="" placeholder="e-mail"></p>
+          <p><input type="password" name="password" value="" placeholder="password"></p>
+          <p><input type="submit" name="submit" value="Se Connecter"></p>
+        </form>
+
+      </article>
+      <aside class="">
+
+      </aside>
+    </div>
+    <footer></footer>
+
+
   </body>
 </html>
