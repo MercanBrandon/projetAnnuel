6e2bd5affd6d7847d937b7d6b6bdd43c4f613125
Brandon MERCAN <mercan.brandon@outlook.fr>
2018-04-26 17:22:11 +0200

    Creation de classe
    squelette de la classe driver,
    debut de la classe user
    connexion : page de test (pour le moment)

--- src/bin/connexion.php
+++ src/bin/connexion.php
@@ -1,15 +1,25 @@
 <?php session_start();
 include_once '_config.php';
 include_once 'class/user.php';
+include_once 'class/driver.php';
+
 
 $mail = $_POST['mail'];
 $password = $_POST['password'];
 
-$usr = new User($mail, $password);
 
 
-var_dump($usr->getName());
-// var_dump($mail);
+$usr = new User($dbh, $mail, $password);
+$driver = new Driver($dbh, $mail, $password);
+
+
+//$usr->addUser('Louison','Laurence', '1997-10-05','0647371613','louison.laurence@gmail.com','123456');
+//$usr->addAdress('Rouen', '63 rue Moïse', '', '76000', 'Rouen');
+
+
+
+//var_dump($usr->getName());
+//var_dump($driver);
 // var_dump($password);
 
 
