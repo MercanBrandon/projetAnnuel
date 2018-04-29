1c4f37ba0c71d0aec8e0f8308724d558e68229d7
MercanBrandon <mercan.brandon@outlook.fr>
2018-04-29 20:12:34 +0200

    add car

---
+++ src/bin/class/car.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ */
+class Car
+{
+  private $marque;
+  private $immatricule;
+  private $modele;
+  
+  function __construct($dbh)
+  {
+  }
+  $Vehicule = new Car($marque, $immatricule, $modele);
+
+
+
+  public function newCar(){
+    // TODO: si la date d'embauche n'est pas définie, la définir, sinon, modifier 
+  }
+
+  public function atributeCar(){
+    // TODO: attribue une voiture au chauffeur courant
+  }
+
+  public function selectCourses(){
+    // TODO: recuperer la liste de course attribuer au chauffeur en cours
+  }
+
+
+
+
+}
+
+ ?>
