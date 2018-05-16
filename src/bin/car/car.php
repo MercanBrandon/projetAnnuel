73bb9bbbf10e1da0dac3f3d270e7fded8886ebec
Unknown <mercan.brandon@outlook.fr>
2018-05-16 13:42:18 +0200

    Car Manager

--- src/bin/car/car.php
+++ src/bin/car/car.php
@@ -4,31 +4,33 @@
  */
 class Car
 {
-  private $marque;
-  private $immatricule;
-  private $modele;
-  
-  function __construct($dbh)
-  {
-  }
-  $Vehicule = new Car($marque, $immatricule, $modele);
+  private $car_id;
+  private $car_immat;
+  private $car_brand;
+  private $car_model;
 
-
-
-  public function newCar(){
-    // TODO: si la date d'embauche n'est pas définie, la définir, sinon, modifier 
+  function __construct(array $donnees){
+    $this->hydrate($donnees);
   }
 
-  public function atributeCar(){
-    // TODO: attribue une voiture au chauffeur courant
+  public function hydrate(array $donnees){
+    foreach ($donnees as $key => $value) {
+      $method = 'set'.ucfirst($key);
+      if (method_exists($this, $method)) {
+        $this->$method($value);
+      }
+    }
   }
 
-  public function selectCourses(){
-    // TODO: recuperer la liste de course attribuer au chauffeur en cours
-  }
+  public function getCar_id(){return $car_id;}
+  public function getCar_immat(){return $car_immat;}
+  public function getCar_brand(){return $car_brand;}
+  public function getCar_model(){return $car_model;}
 
-
-
+  public function setCar_id($car_id){$this->car_id = $car_id;}
+  public function setCar_immat($car_immat){$this->car_immat = $car_immat;}
+  public function setCar_brand($car_brand){$this->car_brand = $car_brand;}
+  public function setCar_model($car_model){$this->car_model = $car_model;}
 
 }
 
