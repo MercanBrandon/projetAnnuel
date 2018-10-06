060f97e0dcbb4500b64e63d7119812ed3aff237d
sonnycabrisseau <sonny.cabrisseau@gmail.com>
2018-10-06 21:47:54 +0200

    dossier api

---
+++ src/api/getdrivers.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once ('../bin/driver/DriverManager.php');
+require_once 'connect.php';
+
+if(isset($_GET['geoPoint']))
+{
+	$driver = new DriverManager($db);
+	header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET");
+    echo json_encode($driver->getDriverPoint($_GET['geoPoint']));
+}
+
+
+
+
+
+?>
