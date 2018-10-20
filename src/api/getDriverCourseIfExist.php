8515f2b4704e5db22771b8010772d20a68795883
Arinouchkine <arinouchkine@gmail.com>
2018-10-20 15:54:11 +0200

    api getDriverCourseIfExist v 2

--- src/api/getDriverCourseIfExist.php
+++ src/api/getDriverCourseIfExist.php
@@ -5,3 +5,23 @@
  * Date: 20/10/2018
  * Time: 15:42
  */
+
+require_once ('../bin/temp/TempManager.php');
+require_once 'connect.php';
+
+
+if($_GET['driver_id'])
+{
+    $tm = new  TempManager($db);
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET");
+    $response = $tm->getDriverCourseIfExist($_GET['driver_id']);
+    echo json_encode($response);
+}
+else{
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET");
+    echo json_encode(false);
+}
