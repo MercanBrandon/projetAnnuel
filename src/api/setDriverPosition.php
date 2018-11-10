90fb37c20215776af5e81f117d5bea24aae1661a
MercanBrandon <mercan.brandon@outlook.fr>
2018-11-10 16:50:18 +0100

    auto change driver position

--- src/api/setDriverPosition.php
+++ src/api/setDriverPosition.php
@@ -6,6 +6,12 @@ require_once 'connect.php';
 // var_dump($_GET);
 
 if (isset($_GET['lng']) && isset($_GET['lat']) && isset($_GET['id'])) {
-  $sql =
+  $sql = $db->prepare("UPDATE driver SET drv_lat = :lat , drv_lng = :lng WHERE drv_id = :drv_id");
+  $sql->execute([
+    'lat' => $_GET['lat'],
+    'lng' => $_GET['lng'],
+    'drv_id' => $_GET['id']
+  ]);
+  
 }
  ?>
